c821bbe1af9d0af34723f0b2bad7c5071eb6d9b9
Ben Cherek <ben_cherek@brown.edu>
2024-12-10 17:29:41 -0500

    added balloon texture

--- src/material_constants/enemy_materials.cpp
+++ src/material_constants/enemy_materials.cpp
@@ -7,14 +7,14 @@
 
 namespace enemy_materials {
     SceneMaterial enemyMaterial = SceneMaterial{
-            SceneColor{0.5f, 0.5f, 0.5f, 1.f}, //ambient
-            SceneColor{0.5f, 0.75f, 0.5f, 1.f}, //diffuse
-            SceneColor{0.5f, 0.75f, 0.5f, 1.f}, //specular
-            1.f, //shininesss
-            SceneColor{0.5f, 0.75f, 0.5f, 1.f}, // Used to weight contribution of reflected ray lighting (via multiplication)
-            SceneColor{0.5f, 0.75f, 0.5f, 1.f}, // Used to weight contribution of reflected ray lighting (via multiplication)
-            1.f, //index of refraction
-            SceneFileMap{},
+            SceneColor{glm::vec4(0.f, 0.f, 0.f, 1.f)}, //ambient
+            SceneColor{glm::vec4(0.f, 0.f, 0.f, 1.f)}, //diffuse
+            SceneColor{glm::vec4(0.f, 0.f, 0.f, 1.f)}, //specular
+            0.f, //shininesss
+            SceneColor{glm::vec4(1.f)}, // Used to weight contribution of reflected ray lighting (via multiplication)
+            SceneColor{glm::vec4(1.f)}, // Used to weight contribution of reflected ray lighting (via multiplication)
+            0.f, //index of refraction
+            SceneFileMap{true, std::string("scenefiles/moretextures/balloon.png"), 1.f, 1.f},
             1.f //blend
     };
     SceneMaterial damagedEnemyMaterial = SceneMaterial{
@@ -25,7 +25,7 @@ namespace enemy_materials {
         SceneColor{1.f, 0.75f, 0.5f, 1.f}, // Used to weight contribution of reflected ray lighting (via multiplication)
         SceneColor{1.f, 0.75f, 0.5f, 1.f}, // Used to weight contribution of reflected ray lighting (via multiplication)
         1.f, //index of refraction
-        SceneFileMap{},
+            SceneFileMap{true, std::string("scenefiles/moretextures/balloon.png"), 1.f, 1.f},
         1.f //blend
 };
 }
