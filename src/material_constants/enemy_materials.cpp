2d6a9a1df88c131131943a28033027625ea2a4cc
L1Z3 <10276179+L1Z3@users.noreply.github.com>
2024-12-09 21:45:49 -0500

    clang 15 fixes

--- src/material_constants/enemy_materials.cpp
+++ src/material_constants/enemy_materials.cpp
@@ -6,15 +6,15 @@
 #include <utils/scenedata.h>
 
 namespace enemy_materials {
-  SceneMaterial enemyMaterial = SceneMaterial(
-  SceneColor(0.5f, 0.5f, 0.5f,1.f), //ambient
-  SceneColor(0.5f, 0.75f, 0.5f,1.f), //diffuse
-  SceneColor(0.5f, 0.75f, 0.5f,1.f), //specular
-  1.f, //shininesss
-  SceneColor(0.5f, 0.75f, 0.5f,1.f), // Used to weight contribution of reflected ray lighting (via multiplication)
-  SceneColor(0.5f, 0.75f, 0.5f,1.f), // Used to weight contribution of reflected ray lighting (via multiplication)
-  1.f, //index of refraction
-  SceneFileMap(),
-  1.f //blend
-  );
+    SceneMaterial enemyMaterial = SceneMaterial{
+            SceneColor{0.5f, 0.5f, 0.5f, 1.f}, //ambient
+            SceneColor{0.5f, 0.75f, 0.5f, 1.f}, //diffuse
+            SceneColor{0.5f, 0.75f, 0.5f, 1.f}, //specular
+            1.f, //shininesss
+            SceneColor{0.5f, 0.75f, 0.5f, 1.f}, // Used to weight contribution of reflected ray lighting (via multiplication)
+            SceneColor{0.5f, 0.75f, 0.5f, 1.f}, // Used to weight contribution of reflected ray lighting (via multiplication)
+            1.f, //index of refraction
+            SceneFileMap{},
+            1.f //blend
+    };
 }
